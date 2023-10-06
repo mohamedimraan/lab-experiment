f530412d0d46cf0adc539bd157093dc4ee479179
mohamedimraan <147144260+mohamedimraan@users.noreply.github.com>
2023-10-06 14:44:23 +0530

    exp 23.java

---
+++ smartengineering/exp 23.java
@@ -0,0 +1,8 @@
+// display Title and Location
+<HEAD>
+<TITLE> Test JavaScript </TITLE>
+<SCRIPT Language="JavaScript">
+document.write(document.title+" <BR>");
+document.write(location.toString());
+</SCRIPT>
+</HEAD>
